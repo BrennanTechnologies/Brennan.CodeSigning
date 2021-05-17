cfee68204e368635276a0ad02a38a2d4b230c86e
Chris Brennan <brennanc@hotmail.com>
2021-05-17 06:19:00 -0400

    Update Brennan.CodeSigning.psm1

--- Brennan.CodeSigning.psm1
+++ Brennan.CodeSigning.psm1
@@ -39,22 +39,3 @@ If (   $True -eq [Boolean]$(Test-Path -Path $PrivatePath -ErrorAction SilentlyCo
         }
     }
 }
-<#
-Try {
-    $script_name = "Abacus-ScriptSigning"
-    $logname = "ScriptSigning.log"
-    $audit_logroot = "\\service02.corp\DFS\SHARES\PSAuditLogs\"
-    $log_logroot = "C:\PSlogs"
-
-    $audit_logpath = Join-Path $(Join-Path $audit_logRoot $script_name) $logname
-    $log_logpath = Join-Path $(Join-Path $log_logroot $script_name) $logname
-
-    Write-Host $audit_logpath
-    Write-Host $log_logpath
-
-    $ScriptSigningLog = Start-Log -LogPath $log_logpath -ScriptName $script_name -Audit True -AuditLogPath $audit_logpath -Global False
-}
-Catch{
-    Throw "Failed to initiate log $($PortalLogObject | Out-String) `n($_.exception)"
-}
-#>
